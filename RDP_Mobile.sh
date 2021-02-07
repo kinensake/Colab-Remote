2cb5eda7bfec00690405943a552481295bb6b750
kinensake <51827494+kinensake@users.noreply.github.com>
2021-02-07 16:42:25 +0700

    Update RDP_Mobile.sh

--- RDP_Mobile.sh
+++ RDP_Mobile.sh
@@ -30,7 +30,7 @@ printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -t 60 -n 50000 -p "Paste Here: " CRP
-CRP+=" --pin=123456"
+
 su - Aevuive -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
