84ab36d0e2b9c9f30ec042f58c9b9c03066da697
kinensake <51827494+kinensake@users.noreply.github.com>
2021-02-07 16:32:57 +0700

    Update RDP_Mobile.sh

--- RDP_Mobile.sh
+++ RDP_Mobile.sh
@@ -1,1 +1,42 @@
 
+#! /bin/bash
+export supass=aevuive
+printf "SUPER USER Pass: aevuive"
+printf "Cho\` xiu\' coi... (Trong luc\' cho\` vao\` day mak dua top: https://surviv.io/) " >&2
+{
+sudo useradd -m Aevuive
+sudo adduser Aevuive sudo
+echo "Aevuive:$supass" | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y
+sudo apt install nomacs -y
+sudo apt install vlc -y
+sudo sed -i 's/geteuid/getppid/' /usr/bin/vlc
+sudo adduser Aevuive chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Complete " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -t 60 -n 50000 -p "Paste Here: " CRP
+CRP+=" --pin=123456"
+su - Aevuive -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+    printf "PIN: 123456"
+else
+    printf "\n\nError Occured " >&2
+fi
