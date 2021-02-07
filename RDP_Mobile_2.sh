fbc7d7da2cbe5a3eda425977a7dd41e86422cd71
kinensake <51827494+kinensake@users.noreply.github.com>
2021-02-07 17:05:46 +0700

    Update RDP_Mobile_2.sh

--- RDP_Mobile_2.sh
+++ RDP_Mobile_2.sh
@@ -2,7 +2,8 @@ su - Aevuive -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
-    printf "\n\nUpgrade Completed " >&2
+    printf "\n\nUpgrade Completed\n " >&2
+    printf "PIN: 123456 " >&2
 else
     printf "\n\nError Occured " >&2
 fi
