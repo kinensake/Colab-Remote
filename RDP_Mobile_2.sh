bbbe9ca15d89735c32379e03da4b226ec3e49cc5
kinensake <51827494+kinensake@users.noreply.github.com>
2021-02-07 16:56:54 +0700

    Create RDP_Mobile_2.sh

---
+++ RDP_Mobile_2.sh
@@ -0,0 +1,8 @@
+su - Aevuive -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+else
+    printf "\n\nError Occured " >&2
+fi
